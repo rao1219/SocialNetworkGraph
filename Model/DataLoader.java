d506b15941442a270e4ade1f400c891b185d5302
rao1219 <441718399@qq.com>
2017-03-22 20:37:54 +0800

    add loader

---
+++ Model/DataLoader.java
@@ -0,0 +1,12 @@
+package Model;
+
+import org.jgrapht.ext.GraphMLImporter;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+
+public class DataLoader {
+	public static void main(String[] args){
+		GraphMLImporter<String, DefaultWeightedEdge> g;
+		
+	}
+}
