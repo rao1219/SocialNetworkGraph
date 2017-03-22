db82aea38b673ac9b3f2c6abda288202bc27cfb7
rao1219 <441718399@qq.com>
2017-03-22 23:27:39 +0800

    text->graph interface

--- Model/GraTest.java
+++ Model/GraTest.java
@@ -10,12 +10,8 @@ import org.jgrapht.graph.DefaultEdge;
 
 public class GraTest {
 	public static void main(String[] args){
-		int[] a = new int[3];
-		int[][] b = new int[2][3];
-		a[1] = 2;
-		a[2] = 1;
-		b[0] = a;
-		b[1] = a;
-		System.out.println(b[1][1]);
+		Graph g = new Graph();
+		g.addEdge("b", "a", 12);
+		g.addEdge("b", "c", 1);
 	}
 }
